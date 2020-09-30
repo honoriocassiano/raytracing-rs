8605e8c8f78a2465152b46d3f0ab9de65806fe91
cassiano <honoriocassiano@gmail.com>
2020-09-30 11:49:44 -0300

    Create the first image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let width: i32 = 256;
+    let height: i32 = 256;
+
+    println!("P3\n{} {}\n255", width, height);
+
+    for line in (0..height).rev() {
+        for column in 0..width {
+            let r: f32 = (column as f32) / ((width - 1) as f32);
+            let g: f32 = (line as f32) / ((height - 1) as f32);
+            let b: f32 = 0.25;
+
+            let ir = (255.999 * r) as i32;
+            let ig = (255.999 * g) as i32;
+            let ib = (255.999 * b) as i32;
+
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
 }
