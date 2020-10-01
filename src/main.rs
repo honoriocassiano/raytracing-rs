6121d9cc4081bc8cae0000446d220485e2d0ccea
cassiano <honoriocassiano@gmail.com>
2020-10-01 09:54:08 -0300

    Add function to write Color to buffer

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
+use std::io::Write;
+
+use vec::Vec3 as Color;
+use vec::Vec3 as Point3;
+
+
 mod vec;
+
+
+fn write_color<T: Write>(out: &mut T , color: &Color) {
+
+	let ir = (255.999 * color.x()) as i32;
+	let ig = (255.999 * color.y()) as i32;
+	let ib = (255.999 * color.z()) as i32;
+
+	out.write(format!("{} {} {}", ir, ig, ib).as_bytes()).unwrap();
+}
+
 
 fn main() {
 
