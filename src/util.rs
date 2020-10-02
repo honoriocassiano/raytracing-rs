0fb1052a5e76e290b74511fe5eabc999bd923198
cassiano <honoriocassiano@gmail.com>
2020-10-01 21:46:55 -0300

    Add function clamp

--- src/util.rs
+++ src/util.rs
@@ -22,3 +22,14 @@ pub fn rand_between(min: f64, max: f64) -> f64 {
 
 	rng.gen_range(min, max)
 }
+
+pub fn clamp(value: f64, min: f64, max: f64) -> f64 {
+
+	if value < min {
+		min
+	} else if value > max {
+		max
+	} else {
+		value
+	}
+}
