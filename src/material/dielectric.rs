6edfdc558890f863b4b7d31f7911724dc71316f1
cassiano <honoriocassiano@gmail.com>
2020-10-02 17:22:35 -0300

    Implement dielectric reflection

--- src/material/dielectric.rs
+++ src/material/dielectric.rs
@@ -1,4 +1,4 @@
-use crate::core::{Color, Ray};
+use crate::core::{Color, Vec3, Ray, dot};
 use crate::util::{rand_unit_vector};
 use crate::hitrecord::BasicHitRecord;
 
@@ -24,7 +24,6 @@ impl Dielectric {
 
 impl Material for Dielectric {
 	fn scatter(&self, in_ray: &Ray, hit: &BasicHitRecord) -> Option<ScatterRecord> {
-
 		let eta_in_over_eta_out = {
 			if hit.front_face() {
 				1.0 / self.refractive_index
@@ -34,11 +33,21 @@ impl Material for Dielectric {
 		};
 
 		let unit_direction = in_ray.direction.normalized();
-		let refracted = unit_direction.refract(&hit.normal(), eta_in_over_eta_out);
 
-		let scatter_record = ScatterRecord{
-			ray: Ray { origin: hit.point(), direction: refracted },
-			attenuation: Color(1.0, 1.0, 1.0)
+		let cos_theta = dot(&(-unit_direction), &hit.normal()).min(1.0);
+		let sin_theta = (1.0 - cos_theta * cos_theta).sqrt();
+
+		let scatter_direction: Vec3 = {
+			if (eta_in_over_eta_out * sin_theta) > 1.0 {
+				unit_direction.reflect(&hit.normal())
+			} else {
+				unit_direction.refract(&hit.normal(), eta_in_over_eta_out)
+			}
+		};
+
+		let scatter_record = ScatterRecord {
+			ray: Ray { origin: hit.point(), direction: scatter_direction },
+			attenuation: Color(1.0, 1.0, 1.0),
 		};
 
 		Some(scatter_record)
