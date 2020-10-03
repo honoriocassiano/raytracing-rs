e8ed0bad565bb73fa8af1b87011df483496b013d
cassiano <honoriocassiano@gmail.com>
2020-10-03 10:08:44 -0300

    Rename member `Camera::origin` to `position`

--- src/camera.rs
+++ src/camera.rs
@@ -4,7 +4,7 @@ use crate::util::degrees_to_radians;
 
 
 pub struct Camera {
-	origin: Point3,
+	position: Point3,
 	lower_left_corner: Point3,
 
 	horizontal: Vec3,
@@ -24,14 +24,14 @@ impl Camera {
 
 		let focal_length: f64 = 1.0;
 
-		let origin = Point3(0.0, 0.0, 0.0);
+		let position = Point3(0.0, 0.0, 0.0);
 		let horizontal = Vec3(viewport_width, 0.0, 0.0);
 		let vertical = Vec3(0.0, viewport_height, 0.0);
 
-		let lower_left_corner = origin - horizontal/2.0 - vertical/2.0 - Vec3(0.0, 0.0, focal_length);
+		let lower_left_corner = position - horizontal/2.0 - vertical/2.0 - Vec3(0.0, 0.0, focal_length);
 
 		Self {
-			origin,
+			position,
 			lower_left_corner,
 			horizontal,
 			vertical
@@ -40,8 +40,8 @@ impl Camera {
 	
 	pub fn ray(&self, u: f64, v: f64) -> Ray {
 		Ray {
-			origin: self.origin,
-			direction: self.lower_left_corner + u*self.horizontal + v*self.vertical - self.origin
+			origin: self.position,
+			direction: self.lower_left_corner + u*self.horizontal + v*self.vertical - self.position
 		}
 	}
 }
