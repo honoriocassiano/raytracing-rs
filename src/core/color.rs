b6810d9d3094df877cd68db0a9f30aa3456f6b20
cassiano <honoriocassiano@gmail.com>
2020-10-04 07:57:30 -0300

    Reformat file

--- src/core/color.rs
+++ src/core/color.rs
@@ -5,14 +5,13 @@ pub use super::vec::Vec3 as Color;
 
 
 pub fn write_color<T: Write>(out: &mut T, pixel_color: Color, samples_per_pixel: i32) {
-
 	let Color(mut r, mut g, mut b) = pixel_color;
 
 	let scale = 1.0 / samples_per_pixel as f64;
 
-	r = (r *scale).sqrt();
-	g = (g *scale).sqrt();
-	b = (b *scale).sqrt();
+	r = (r * scale).sqrt();
+	g = (g * scale).sqrt();
+	b = (b * scale).sqrt();
 
 	let ir = (255.999 * clamp(r, 0.0, 0.999)) as i32;
 	let ig = (255.999 * clamp(g, 0.0, 0.999)) as i32;
