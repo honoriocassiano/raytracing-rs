6a512c01f9a96147142ab96051732488a0240e21
cassiano <honoriocassiano@gmail.com>
2020-11-07 16:55:41 -0300

    Add some tests to Vec3

--- src/core/geometry/vec3.rs
+++ src/core/geometry/vec3.rs
@@ -215,3 +215,129 @@ impl fmt::Display for Vec3 {
 }
 
 pub use Vec3 as Point3;
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    const EPSILON: f64 = 1e-08;
+
+    fn almost_equal(v1: f64, v2: f64) -> bool {
+        (v1 - v2).abs() < EPSILON
+    }
+
+    fn almost_equal_vec(v1: Vec3, v2: Vec3) -> bool {
+        let diff_x = (v1.0 - v2.0).abs();
+        let diff_y = (v1.1 - v2.1).abs();
+        let diff_z = (v1.2 - v2.2).abs();
+
+        (diff_x + diff_y + diff_z) < EPSILON
+    }
+
+    // Tests for getters
+    #[test]
+    fn must_set_coordinates() {
+        let expected_x = 0.597;
+        let expected_y = 1.47856;
+        let expected_z = 15.857;
+
+        let vec = Vec3(expected_x, expected_y, expected_z);
+
+        assert_eq!(expected_x, vec.x());
+        assert_eq!(expected_y, vec.y());
+        assert_eq!(expected_z, vec.z());
+    }
+
+    // Tests for Vector trait
+
+    #[test]
+    fn must_have_length_zero() {
+        let expected = 0.0;
+
+        assert!(almost_equal(expected, Vec3::zero().length()));
+    }
+
+    #[test]
+    fn must_have_length_one() {
+        let expected = 1.0;
+        let vec = Vec3(1.0, 0.0, 0.0);
+
+        assert!(almost_equal(expected, vec.length()));
+    }
+
+    #[test]
+    fn must_have_sq_length_zero() {
+        let expected = 0.0;
+
+        assert!(almost_equal(expected, Vec3::zero().sq_length()));
+    }
+
+    #[test]
+    fn must_have_sq_length_one() {
+        let expected = 1.0;
+        let vec = Vec3(1.0, 0.0, 0.0);
+
+        assert!(almost_equal(expected, vec.sq_length()));
+    }
+
+    #[test]
+    fn must_have_normalized_length_one() {
+        let expected = 1.0;
+        let vec = Vec3(1.0, 5.0, 2.0);
+
+        assert!(almost_equal(expected, vec.normalized().length()));
+
+        let mut mut_vec = vec;
+        mut_vec.normalize();
+
+        assert!(almost_equal(expected, mut_vec.length()));
+    }
+
+    #[test]
+    fn must_be_equal_sum() {
+        let expected = Vec3(1.0, 5.3, 2.0);
+
+        let vec1 = Vec3(0.0, 2.1, 1.9);
+        let vec2 = Vec3(1.0, 3.2, 0.1);
+
+        assert!(almost_equal_vec(expected, vec1 + vec2));
+
+        let mut mut_vec1 = vec1;
+        mut_vec1 += vec2;
+
+        assert!(almost_equal_vec(expected, mut_vec1));
+    }
+
+    #[test]
+    fn must_be_equal_neg() {
+        let expected = Vec3(0.5, -0.9, 0.0);
+
+        let vec = Vec3(-0.5, 0.9, 0.0);
+
+        assert!(almost_equal_vec(expected, -vec));
+    }
+
+    #[test]
+    fn must_return_coordinate() {
+        let expected_0 = 1.0;
+        let expected_1 = 5.48;
+        let expected_2 = -0.48;
+
+        let vec = Vec3(expected_0, expected_1, expected_2);
+
+        assert_eq!(expected_0, vec[0]);
+        assert_eq!(expected_1, vec[1]);
+        assert_eq!(expected_2, vec[2]);
+    }
+
+    #[test]
+    #[should_panic(expected = "Index 3 is not in Vec3")]
+    fn must_panic_on_invalid_index() {
+        let vec = Vec3::zero();
+
+        vec[3];
+    }
+
+    // TODO Add Test to other operations
+}
